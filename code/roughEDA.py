ccd2f242c568820c8127e538f93bb03035a507bb
banjopickin <yihualeng@gmail.com>
2015-12-14 22:55:16 -0800

    created serval EDA functions

--- code/roughEDA.py
+++ code/roughEDA.py
@@ -10,6 +10,7 @@ def simple_bar(col, size = (8,8)):
     '''
     generate a bar-chart to visualize counts of categorical variable
     :param col: specific column
+    :param size: figuresize, tuple
     :return: bar-chart
     '''
     val_counts = col.value_counts()
@@ -28,5 +29,37 @@ def simple_bar(col, size = (8,8)):
                 ha='center', va='bottom')
 
 
+def stack_bar(colx,coly,size = (8,8)):
+    '''
+    create a stacked bar chart
+    :param colx: varialbes show on x-axis
+    :param coly: variables counts show on y-axis
+    :param size: figure size
+    :return: stacked  bar chart
+    '''
+    temp_df = pd.crosstab(colx,coly,dropna=False)
+    temp_df.plot(kind='bar', figsize = size)
+
+def h_bar(col_group,col_split, size = (8,8)):
+    '''
+    create a unstack horizontal bar chart
+    :param colx: varialbes to group
+    :param coly: variables to split within each group
+    :param size: figsize
+    :return: horizontal bar chart
+    '''
+    temp_df = pd.crosstab(col_group,col_split,dropna=False)
+    temp_df.plot(kind = 'barh', figsize = size)
+
+
+def pie_chart(col_fracs, col_lab, size = (20,8)):
+    '''
+    generate a pie chart
+    :param col_fracs: variable value counts to show on pie chart
+    :param col_lab: each slice label
+    :return: pie chart
+    '''
+    temp_df = pd.crosstab(col_lab, col_fracs, dropna=False)
+    temp_df.plot(kind = 'pie',subplots = True, figsize = size,autopct='%.2f')
 
 
