9072d1ea973d882e2d9f343132b0cd584bd983e1
banjopickin <yihualeng@gmail.com>
2015-12-18 23:02:58 -0800

    bug fixed, need to add doc strings

--- code/survey_processor.py
+++ code/survey_processor.py
@@ -1,11 +1,13 @@
 '''
 Treat the entire survey data a class object. Use a series of functions to process the data for machine learnig step.
+In the mean time,
 '''
 
 from __future__ import division
 import pandas as pd
 from organize import *
 import numpy as np
+import pdb
 
 class survey(object):
     '''
@@ -19,7 +21,7 @@ class survey(object):
         :output: read data, list columns to drop, list rows to filter
         '''
         self.dir = dir
-        self.data = pd.read_excel(dir + '/GSS.xls')
+        self.raw_data = pd.read_excel(dir + '/GSS.xls')
         self.cols_drop = ['babies','preteen','wrkstat','sphrs2']
         self.row_drop = ['babies','preteen']
         self.survey_cols = ['degree', 'mawrkgrw', 'incom16', 'natspac',
@@ -45,17 +47,17 @@ class survey(object):
         :return: none
         '''
         variables = extract_variables(self.dir + '/GSS.sps')
-        replace_variables(self.data,variables)
+        replace_variables(self.raw_data,variables)
 
     def _filter(self):
         '''
         select population with babies or preteen children
-        :return: none
+        :return: data frame
         '''
         # filter women with babies or preteen kids
-        temp = self.data[self.data[self.row_drop].apply(lambda x:x>0).sum(axis =1)>0]
+        temp = self.raw_data[self.raw_data[self.row_drop].apply(lambda x:x>0).sum(axis =1)>0]
         # drop 'no answer' respondent
-        self.data = temp[temp[self.row_drop].apply(lambda x: x!= 'No answer').sum(axis = 1) >0]
+        return temp[temp[self.row_drop].apply(lambda x: x!= 'No answer').sum(axis = 1) >0]
 
     def _employ(self):
         '''
@@ -67,7 +69,7 @@ class survey(object):
         self.data['employed'] = self.data.wrkstat.apply(lambda x: x in employed)
 
 
-    def rough_process(self):
+    def _rough_process(self):
         '''
         steps:
         1. simplify variable labels
@@ -76,10 +78,32 @@ class survey(object):
         4. drop columns
         :return: none
         '''
-        self._simp_var()
-        self._filter()
-        self._employ()
-        self.data.drop(self.cols_drop,axis=1,inplace = True)
+        variables = extract_variables(self.dir + '/GSS.sps')
+        a = [i.upper() for i in self.raw_data.columns]
+        if set(a) != set(variables.values()):
+            self._simp_var()
+        df = self._filter()
+        employed = ['Working fulltime', 'Working parttime']
+        df['employed'] = self.raw_data.wrkstat.apply(lambda x: x in employed)
+        df.drop(self.cols_drop,axis=1,inplace = True)
+        return df
+
+    def copy_rough(self):
+        '''
+        need this for eda part
+        :return: self.data
+        '''
+        #pdb.set_trace()
+        variables = extract_variables(self.dir + '/GSS.sps')
+        a = [i.upper() for i in self.raw_data.columns]
+        if set(a) != set(variables.values()):
+            self._simp_var()
+        df = self._filter()
+        employed = ['Working fulltime', 'Working parttime']
+        df['employed'] = df.wrkstat.apply(lambda x: x in employed)
+        df.drop(self.cols_drop,axis=1,inplace = True)
+        self.data = df.copy()
+
 
     def _impute_cols(self):
         '''
@@ -88,7 +112,7 @@ class survey(object):
         '''
         df = pd.DataFrame()
         for col in self.impute_cols:
-            df[col] = impute_median(self.data[col], self.bs)
+            df[col] = impute_median(self.raw_data[col], self.bs)
         return df
 
 
@@ -100,11 +124,11 @@ class survey(object):
         a = pd.DataFrame()
         for col in self.par_impute_cols:
             if col == "speduc":
-                a[col] = sub_impute(self.data[col],["No answer","Don't know"], self.bs)
+                a[col] = sub_impute(self.raw_data[col],["No answer","Don't know"], self.bs)
                 a[col].replace("Not applicable", -99, inplace = True)
                 a[col + "_not_app"] = a[col].apply(lambda x: x==99)
             else:
-                a[col] = sub_impute(self.data[col],['No answer'], self.bs)
+                a[col] = sub_impute(self.raw_data[col],['No answer'], self.bs)
                 a[col].replace("Not applicable",-99, inplace = True)
                 a[col + "_not_app"] = a[col]== -99
                 a[col].replace("Don't know", -98, inplace = True)
@@ -120,10 +144,10 @@ class survey(object):
         :return: new data frame. This data frame can be used for random forest or any decision tree models
         '''
         # rough process
-        self.rough_process()
-        df = self.data
+        df = self._rough_process()
         impt_cols = self._impute_cols()
         par_imp_cols = self._partial_impute_bin()
+        #pdb.set_trace()
         self.fin_data = pd.concat([impt_cols,par_imp_cols,df[self.survey_cols]], axis=1)
 
 
