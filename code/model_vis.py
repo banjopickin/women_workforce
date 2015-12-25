812d7e2a4528bd9ea474b24ecf63bbe7dc56e1b7
banjopickin <yihualeng@gmail.com>
2015-12-24 20:51:06 -0800

    add new functions

--- code/model_vis.py
+++ code/model_vis.py
@@ -202,7 +202,7 @@ def km_emp_mean(df_pca,krange,empcol,crtcol):
     '''
     lis = []
     df = pd.concat([empcol,crtcol],axis=1)
-    for k in xrange(5,krange):
+    for k in xrange(2,krange):
         km = KMeans(n_clusters=k,random_state=0)
         km.fit(df_pca)
         df['cluster'] = km.labels_
@@ -214,35 +214,34 @@ def km_emp_mean(df_pca,krange,empcol,crtcol):
         print "{} out of {} clusters split the target ideally. Good cluster rate: {}".format(res.shape[0],k,res.shape[0]/k)
         print '*'*20
         lis.append(res.shape[0]/k)
-    plt.plot(range(5,krange),lis,lw = 2)
+    plt.plot(range(2,krange),lis,lw = 2)
     plt.xlabel('K')
     plt.ylabel('good cluster rate')
 
 
-# def km_emp(df_pca,krange,empcol,crtcol):
-#     '''
-#     apply kmeans to pca-processed dataframe, with a range of k. Then check with k yields clearest employed rate
-#     and correct rate.
-#     :param df_pca: numpy array
-#     :param krange: int, range of k
-#     :param empcol: panda series
-#     :param crtcol: panda series
-#     :return: plot
-#     '''
-#     lisT = []
-#     lisF = []
-#     df = pd.concat([empcol,crtcol],axis=1)
-#     for k in xrange(5,krange):
-#         km = KMeans(n_clusters=k)
-#         km.fit(df_pca)
-#         df['cluster'] = km.labels_
-#         temp = df.groupby('cluster').agg(np.mean)
-#         resT = temp[(temp['employed'] >0.7)) &(temp['correct'] >0.7) ]
-#         resF = temp[(temp['employed'] < 0.4) &(temp['correct'] >0.7) ]
-#         lisT.append(resT.shape[0])
-#         lisF.append(resF.shape[0])
-#     plt.plot(range(5,krange),lisT,lw = 2, label = 'employed')
-#     plt.plot(range(5,krange),lisF, lw = 2, label = 'unemployed')
-#     plt.legend
-#     plt.xlabel('K')
-#
+def km_emp(df_pca,krange,empcol,crtcol):
+    '''
+    apply kmeans to pca-processed dataframe, with a range of k. Then check with k yields clearest employed rate
+    and correct rate.
+    :param df_pca: numpy array
+    :param krange: int, range of k
+    :param empcol: panda series
+    :param crtcol: panda series
+    :return: plot
+    '''
+    lisT = []
+    lisF = []
+    df = pd.concat([empcol,crtcol],axis=1)
+    for k in xrange(5,krange):
+        km = KMeans(n_clusters=k)
+        km.fit(df_pca)
+        df['cluster'] = km.labels_
+        temp = df.groupby('cluster').agg(np.mean)
+        resT = temp[(temp['employed'] >0.7) &(temp['correct'] >0.7) ]
+        resF = temp[(temp['employed'] < 0.4) &(temp['correct'] >0.7) ]
+        lisT.append(resT.shape[0])
+        lisF.append(resF.shape[0])
+    plt.plot(range(5,krange),lisT,lw = 2, label = 'employed')
+    plt.plot(range(5,krange),lisF, lw = 2, label = 'unemployed')
+    plt.legend()
+    plt.xlabel('K')
