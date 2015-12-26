00b2a6b17d8dc7ee81d2ab636b6d48bada82dccb
banjopickin <yihualeng@gmail.com>
2015-12-26 11:44:46 -0800

    add some functions

--- code/fc.py
+++ code/fc.py
@@ -2,8 +2,10 @@
 This document contains the functions for feature contribution study
 '''
 
+from __future__ import division
 import pandas as pd
 import numpy as np
+from collections import Counter,OrderedDict
 
 def sort_feature_means(df,topn):
     '''
@@ -16,3 +18,16 @@ def sort_feature_means(df,topn):
     m = df.mean(axis = 0).values
     s = sorted(zip(m,df.columns), key= lambda x: abs(x[0]), reverse=True)
     return s[:topn]
+
+order = lambda col: OrderedDict(sorted(Counter(col).items(),key = lambda x: x[1], reverse=True))
+#rank_2nd = lambda col: OrderedDict(sorted(Counter(col).items(),key = lambda x: x[1], reverse=True)).items()[1]
+
+def rank_1st(col):
+    dict = OrderedDict(sorted(Counter(col).items(),key = lambda x: x[1], reverse=True))
+    x = dict.items()[0]
+    return x[0],round(x[1]/sum(dict.values()),2)
+
+def rank_2nd(col):
+    dict = OrderedDict(sorted(Counter(col).items(),key = lambda x: x[1], reverse=True))
+    x = dict.items()[1]
+    return x[0],round(x[1]/sum(dict.values()),2)
