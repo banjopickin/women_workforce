3fe41cd996528ab35895340c8819cb575942456d
banjopickin <yihualeng@gmail.com>
2015-12-29 22:32:47 -0800

    add some functions

--- code/clusterEDA.py
+++ code/clusterEDA.py
@@ -2,11 +2,12 @@
 This file contains functions for visualising clusters
 '''
 
-import plotly.plotly as py
-import plotly.graph_objs as go
+from __future__ import division
+
 import matplotlib.pylab as plt
 import numpy as np
 import pandas as pd
+import scipy.stats as scs
 
 
 def one_pub_hist_mat(df,cluster_id, variable,norm = True):
@@ -96,3 +97,65 @@ def one_res_bar(df,cluster_id, variable, size = (10,5)):
 
     plt.legend(title = variable, loc='center left', bbox_to_anchor=(1, 0.5))
     plt.show()
+
+def two_bar(df, cluster1,cluster2,variable, size = (10,5)):
+    '''
+    compare two clusters on same variable
+    :param df: data fram
+    :param cluster1: int, cluster id
+    :param cluster2: int, cluster id
+    :param variable: string, column name
+    :param size: figure size
+    :return:
+    '''
+    df = df.copy()
+    df.replace('Not applicable',np.nan,inplace = True)
+    c1 = df[df.cluster == cluster1][variable].value_counts()
+    c2 = df[df.cluster == cluster2][variable].value_counts()
+    index1 = "Cluster_" + str(cluster1)
+    index2 = "Cluster_" + str(cluster2)
+    temp = pd.DataFrame([c1,c2], index= [index1, index2])
+    temp_n = temp.apply(lambda x: x/x.sum(), axis =1)
+    ax = temp_n.plot(kind = 'bar', width = 0.9, figsize = size)
+    for p in ax.patches:
+        height = p.get_height()
+        ax.text(p.get_x(), height+0.01,'%1.2f'%(height))
+
+    plt.legend(title = variable, loc='center left', bbox_to_anchor=(1, 0.5))
+    plt.show()
+    return temp
+
+
+def cluster_ztest(df,answer, two_tail = False):
+    '''
+    conduct proportional z-test on two cluster
+    :param df: data frame. in this case, contigency table generated from functions above
+    :param answer: string, column name
+    :return: float, float. z-score and p-value.
+    '''
+    na,nb = df.sum(axis =1)
+    va,vb = df[answer]
+    pa = va/na
+    pb = vb/nb
+    pooled = (va+vb)/(na+nb)
+    s = np.sqrt(pooled*(1-pooled)*(1/na+1/nb))
+    z = (pa-pb)/s
+    if two_tail:
+        p = (1 - scs.norm.cdf(abs(z)))*2
+    else:
+        p = 1- scs.norm.cdf(abs(z))
+    return z,p
+
+def cluster_ztest_df(df):
+    '''
+    conduct z-test, on entire data frame. column by column
+    :param df: data frame
+    :return: lis of tuples
+    '''
+    lis = []
+    for c in df.columns:
+        p = cluster_ztest(df,c)[1]
+        # only return significant ones
+        if p <0.05:
+            lis.append((c,round(p,2)))
+    return sorted(lis, key = lambda x: x[1])
